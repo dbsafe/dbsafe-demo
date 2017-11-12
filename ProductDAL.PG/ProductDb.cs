c2e449b0566e9c25de608375bec11e05847298af
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2017-11-12 10:28:06 -0600

    Added projects: ProductDAL.PG and ProductDAL.PG.Tests.

---
+++ ProductDAL.PG/ProductDb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain = ProductBL.Domain;
+
+namespace ProductDAL.PG
+{
+    public class ProductDb : Domain.IProductDb
+    {
+        public int AddProduct(Domain.Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Domain.Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Domain.Product GetProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Domain.ProductSummary> GetProductSummaries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Domain.Supplier> GetSuppliers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateSupplier(Domain.Supplier supplier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
