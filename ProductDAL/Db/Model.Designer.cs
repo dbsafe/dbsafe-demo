e85aa1d996f26180766eb05aa32030b5d318ede4
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2022-02-15 07:29:41 -0500

    Updated packages.

--- ProductDAL/Db/Model.Designer.cs
+++ ProductDAL/Db/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Repos\GitHub\DbIntegrationTest\db-integration-test\ProductDAL\Db\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Repos\dbsafe-demo\ProductDAL\Db\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
